86edcd30f977439a248bd6064793a73cf9e55f8e
sivaven <sivaven7@gmail.com>
2017-03-26 15:08:39 -0400

    init spike pattern classifier

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+spike.csv takes 3 lines of input!
+line 1: input current, current duration
+line 2: spike times
+line 3: swa amplitude (typically >5mV for bursting types)
+*********************************************
+Open command prompt
+execute: java -jar classifySpikePattern.jar
+*********************************************
---
+++ src/ec/app/izhikevich/inputprocess/labels/PatternFeatureID.java
@@ -0,0 +1,48 @@
+package ec.app.izhikevich.inputprocess.labels;
+
+public enum PatternFeatureID {
+	/*
+	 * non constraints
+	 */
+	IDX_NOTE,
+	INCLUDE,
+	pattern_class,
+	compartment,
+	type,
+	current,
+	time_min,
+	current_duration,
+	eval,  
+	pattern_weight,
+	/*
+	 * Constraints
+	 */
+	n_spikes, 
+	//isis,	// currently, no range allowed
+	avg_isi, 
+	fsl, 
+	pss, 
+	sfa_linear_m1, // currently, no range allowed
+	sfa_linear_b1, // currently, no range allowed
+	n_sfa_isis1,  // currently, no range allowed
+	
+	sfa_linear_m2, // currently, no range allowed
+	sfa_linear_b2, // currently, no range allowed
+	n_sfa_isis2,  // currently, no range allowed
+	
+	non_sfa_avg_isi, 
+	sub_ss_voltage, 
+	sub_ss_voltage_sd,
+	vmin_offset,
+	time_const,
+	valid_max_v,
+	
+	nbursts,
+	bursts,
+	stuts,
+	
+	dendMirrorSpikes,
+	
+	rebound_VMax,
+	swa
+}
